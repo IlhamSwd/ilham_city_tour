f3fae8d8c641ec56cef353f0e185a519332ce98e
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-11-02 09:27:56 +0700

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -44,20 +44,20 @@ class HomeScreen extends StatelessWidget {
                     height: 300,
                     width: 150,
                   )
-                  const SizedBox(height: 16, width: 16,),
-                  Image.network(
-                    'https://picsum.photos/id/164/300/200',
-                    fit: BoxFit.cover,
-                    height: 300,
-                    width: 150,
-                  )
-                  const SizedBox(height: 16, width: 16,),
-                  Image.network(
-                    'https://picsum.photos/id/174/300/200',
-                    fit: BoxFit.cover,
-                    height: 300,
-                    width: 150,
-                  )
+                  // const SizedBox(height: 16, width: 16,),
+                  // Image.network(
+                  //   'https://picsum.photos/id/164/300/200',
+                  //   fit: BoxFit.cover,
+                  //   height: 300,
+                  //   width: 150,
+                  // )
+                  // const SizedBox(height: 16, width: 16,),
+                  // Image.network(
+                  //   'https://picsum.photos/id/174/300/200',
+                  //   fit: BoxFit.cover,
+                  //   height: 300,
+                  //   width: 150,
+                  // )
                 ],
               ))
 
@@ -71,49 +71,3 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
